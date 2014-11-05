1c5d4697cdd547cc66d921da0004d7695a96a2c4
Roland Kuhn <rk@rkuhn.info>
2014-11-05 13:50:31 +0100

    no solution

---
+++ src/main/scala/com/rolandkuhn/guarantees/Main.scala
@@ -0,0 +1,75 @@
+import akka.actor.Props
+import akka.util.Timeout
+import akka.actor.RootActorPath
+import akka.actor.Identify
+import akka.actor.ActorIdentity
+import scala.concurrent.Future
+
+package akka.remote {
+  package object rkextras {
+    import akka.remote.transport.FailureInjectorTransportAdapter._
+    def DropIt(addr: akka.actor.Address, probability: Double) = One(addr, Drop(probability, probability))
+  }
+}
+
+package com.rolandkuhn.guarantees {
+
+  import akka.actor.ActorSystem
+  import scala.concurrent.Await
+  import scala.concurrent.duration._
+  import com.typesafe.config.ConfigFactory
+  import akka.actor.Address
+  import akka.remote.testconductor.TestConductor
+  import akka.remote.rkextras._
+  import akka.pattern.ask
+  import scala.reflect.classTag
+
+  object Main extends App {
+
+    def system(addr: Address) =
+      ActorSystem(addr.system, ConfigFactory
+        .parseString(s"""akka.remote.netty.tcp{hostname="${addr.host.get}"\nport=${addr.port.get}}""")
+        .withFallback(ConfigFactory.load()))
+
+    val addressA = Address("akka.gremlin.tcp", "A", "127.0.0.1", 6661)
+    val addressB = Address("akka.gremlin.tcp", "B", "127.0.0.1", 6662)
+
+    val sysA = system(addressA)
+    val sysB = system(addressB)
+
+    implicit val timeout = Timeout(10.seconds)
+    import sysA.dispatcher
+
+    sysB.actorOf(Props(new Receiver), "receiver")
+
+    // make sure that remoting works
+    val recvPath = RootActorPath(addressB) / "user" / "receiver"
+    val recv =
+      Await.result(sysA.actorSelection(recvPath) ? Identify(42) collect {
+        case ActorIdentity(42, Some(ref)) => ref
+      }, 1.second)
+
+    val send = sysA.actorOf(Props(new Sender(recv)), "sender")
+
+    Await.result(TestConductor(sysA).transport.managementCommand(DropIt(addressB, 0.2)), 5.seconds)
+
+    val futures =
+      "The quick brown fox jumps over the lazy dog"
+        .split(' ').toList
+        .map(word =>
+          send ? Sender.Send(word) flatMap {
+            case s: Sender.Sent => send ? Sender.AwaitConfirmation(s.confirmationId)
+          } map {
+            case _ => s"confirmed: $word"
+          } recover {
+            case ex: Throwable => s"missing                 ($word)"
+          })
+
+    Future.sequence(futures) map (_.mkString("\n  ", "\n  ", "\n")) onComplete { x =>
+      println(x)
+      sysB.shutdown()
+      sysA.shutdown()
+    }
+  }
+
+}
---
+++ src/main/scala/com/rolandkuhn/guarantees/Sender.scala
@@ -0,0 +1,77 @@
+package com.rolandkuhn.guarantees
+
+import akka.persistence.PersistentActor
+import akka.actor.ActorRef
+import akka.persistence.AtLeastOnceDelivery
+import java.time.LocalDateTime
+import scala.concurrent.duration._
+import akka.actor.ActorPath
+import akka.actor.Identify
+import akka.actor.ActorIdentity
+import akka.actor.Stash
+import akka.actor.ReceiveTimeout
+
+object Sender {
+  case class Send(text: String)
+  case class Sent(text: String, confirmationId: Int)
+  case class AwaitConfirmation(id: Int)
+  case class Confirmed(id: Int)
+  case class InternalConfirmation(seq: Int, id: Long)
+}
+
+class Sender(target: ActorRef) extends PersistentActor with AtLeastOnceDelivery with Stash {
+  import Sender._
+  import context.dispatcher
+
+  def log(msg: String) = println(s"[${LocalDateTime.now}] send: $msg")
+
+  override def persistenceId = "sender"
+  override def redeliverInterval = 1.second
+
+  var nextSeq = 0
+  var outstanding = Map.empty[Int, Set[ActorRef]]
+
+  def receiveCommand = {
+    case Send(text) =>
+      log(s"sending '$text' with seq=$nextSeq")
+      persist(Sent(text, nextSeq)) { s =>
+        sender() ! s
+        send(text)
+      }
+    case AwaitConfirmation(seq) =>
+      outstanding get seq match {
+        case Some(set) => outstanding += seq -> (set + sender())
+        case None      => sender() ! Confirmed(seq)
+      }
+    case rc @ Receiver.Confirmed(seq, id) =>
+      log(rc.toString)
+      persist(InternalConfirmation(seq, id)) { _ =>
+        confirm(seq, id)
+      }
+  }
+
+  def send(text: String): Unit = {
+    target ! Receiver.Important(text, nextSeq, 0L)
+    outstanding += nextSeq -> Set.empty
+    nextSeq += 1
+  }
+
+  def confirm(seq: Int, id: Long): Unit = {
+    outstanding get seq match {
+      case Some(set) =>
+        val c = Confirmed(seq)
+        set foreach (_ ! c)
+        outstanding -= seq
+      case None =>
+    }
+  }
+
+  def receiveRecover = {
+    case Sent(text, seq) =>
+      send(text)
+      assert(nextSeq == seq + 1)
+    case InternalConfirmation(seq, id) =>
+      confirm(seq, id)
+      
+  }
+}
